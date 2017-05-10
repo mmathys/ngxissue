eed0f4289d0352bdcafae0accd62edba4c85ec50
Max Mathys <max.mathys@swisscom.com>
2017-05-10 17:49:48 +0200

    hi!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpModule, Http } from '@angular/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 
 export { AppComponent };
+export function createTranslateLoader(http: Http) {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +22,16 @@ export { AppComponent };
       { path: '', loadChildren: './home/home.module#HomeModule' },
       { path: 'about', loadChildren: './about/about.module#AboutModule' },
       { path: '**', redirectTo: '', pathMatch: 'full' },
-    ])
+    ]),
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [Http]
+      }
+    })
+
   ],
   exports: [AppComponent],
   providers: [],
